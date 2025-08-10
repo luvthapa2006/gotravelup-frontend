4b543b779038a0af92110d8b8c22d53768720236
luvthapa2006 <thapaluv1029@gmail.com>
2025-08-10 07:26:18 +0530

    Update frontend with latest local changes

--- js/auth.js
+++ js/auth.js
@@ -3,7 +3,7 @@
 // =============================
 
 // Change this if backend URL changes
-const API_BASE = 'https://gotravelup-backend.onrender.com';
+const API_BASE = '';
 
 class AuthManager {
     constructor() {
